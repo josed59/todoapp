0d4bb42947bb98378b3123c2b95d81d65bc0084b
jodaraca <josedavidrc59@gmail.com>
2022-10-17 22:44:35 -0300

    deploy fix 3.3

---
+++ build/precache-manifest.6f011bfd7ec5aee4e8f66bcb0fa0698d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0b352a0825ca5352855",
+    "url": "/todoapp/build/static/css/main.2dea924c.chunk.css"
+  },
+  {
+    "revision": "b0b352a0825ca5352855",
+    "url": "/todoapp/build/static/js/main.b0b352a0.chunk.js"
+  },
+  {
+    "revision": "18dd41c77374d8e00c74",
+    "url": "/todoapp/build/static/js/1.18dd41c7.chunk.js"
+  },
+  {
+    "revision": "94fd607aeb85327b1dc9",
+    "url": "/todoapp/build/static/js/runtime~main.94fd607a.js"
+  },
+  {
+    "revision": "74a2d52c8245d07172c8a75426d241d5",
+    "url": "/todoapp/build/static/media/check-mark.74a2d52c.png"
+  },
+  {
+    "revision": "269afb76c0fe811c527d7104fbd981bd",
+    "url": "/todoapp/build/index.html"
+  }
+];
