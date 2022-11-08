5a718f250800cd7112250af52610f433be1c4bab
jodaraca <josedavidrc59@gmail.com>
2022-11-08 13:01:19 -0300

    Updates

---
+++ precache-manifest.8218da9ba561efbc6846ab70f4997105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "766391140b57cd4f29f5",
+    "url": "/todoapp/build/static/css/main.a4c9dcef.chunk.css"
+  },
+  {
+    "revision": "766391140b57cd4f29f5",
+    "url": "/todoapp/build/static/js/main.76639114.chunk.js"
+  },
+  {
+    "revision": "18dd41c77374d8e00c74",
+    "url": "/todoapp/build/static/js/1.18dd41c7.chunk.js"
+  },
+  {
+    "revision": "94fd607aeb85327b1dc9",
+    "url": "/todoapp/build/static/js/runtime~main.94fd607a.js"
+  },
+  {
+    "revision": "74a2d52c8245d07172c8a75426d241d5",
+    "url": "/todoapp/build/static/media/check-mark.74a2d52c.png"
+  },
+  {
+    "revision": "14c0011e067a3e1fa3e2758a8a3e1643",
+    "url": "/todoapp/build/index.html"
+  }
+];
